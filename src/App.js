f5c876b9ea09882c32856104ad04f3f8956ceade
5910110298 <5910110298@psu.ac.th>
2018-09-20 10:44:55 +0700

    Arry Hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import CharacterCard from './CharacterCard';
-class App extends Component {
-  render() {
-    return (
-      <div>
-       <CharacterCard value="h"/>
-       <CharacterCard value="i"/>
+import React, { Component }from 'react';
+import './App.css';
+import CharacterCard from "./CharacterCard";
+
+const word="Hello";
+class App extends Component{
+  render(){
+    return(
+      <div>{
       
+        Array.from(word).map((c,i)=><CharacterCard value={c} key={i}/>)
+      }
       </div>
     );
   }
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,3 +1,7 @@
+
+import React, { Component } from 'react';
+import './App.css';
+//import CharacterCard from './CharacterCard';
 export default class CharacterCard extends Component{
     render(){
         return(
