9ce57ce7bd5533b00bde00353820f39d438b07a7
5910110298 <32356292+5910110298@users.noreply.github.com>
2018-09-21 17:37:55 +0700

    change word

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "hello";
+const word = "einstein";
 class App extends Component{
   render(){
     return(
